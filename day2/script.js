a85ae1e742699dbd57b1db417e0dbc42dd392d41
Paige Andrews <pandrews25@yahoo.com>
2019-09-27 13:35:31 -0400

    arrays and database practice

---
+++ day2/script.js
@@ -0,0 +1,38 @@
+// let age = Number(prompt(" How old are you?"));
+
+// if (age < 18){
+//     alert( "Sorry! You're too young!");
+// } else if (age === 18){
+//     alert("Congrats on first year of driving!");
+// } else {
+//     alert("Ride on!");
+// }
+
+// function multiply(a,b) {
+//     return a*b;
+// }
+// alert(multiply(4,3));
+
+// function checkDriverAge(age){
+// let age = Number(prompt(" How old are you?"));
+
+//  if (age < 18){
+//     return( "Sorry! You're too young!");
+//  } else if (age === 18){
+//      return("Congrats on first year of driving!");
+//  } else {
+//      return("Ride on!");
+//  }
+// }
+// checkDriverAge();
+
+// function checkDriverAge() {
+//     let age = prompt("What is your age?")
+// 	if (Number(age) < 18) {
+// 		return "Sorry, you are too yound to drive this car. Powering off";
+// 	} else if (Number(age) > 18) {
+// 		return "Powering On. Enjoy the ride!";
+// 	} else if (Number(age) === 18) {
+// 		return "Congratulations on your first year of driving. Enjoy the ride!";
+// 	}
+// } console.log(checkDriverAge());
--- day3/script.js
+++ day3/script.js
@@ -2,6 +2,14 @@ let database = [
     {
         username: "conny",
         password: "supersecret"
+    },
+    {
+        username:"Eren",
+        password: "mikasalovesme"
+    },
+    {
+        username:"Hange",
+        password: "titans4lyfe"
     }
     
 ];
@@ -17,15 +25,27 @@ let newsFeed = [
     }
 ];
 
-let userNamePrompt = prompt("What is your username?");
-let passwordPrompt = prompt("What is your password?");
+function isUserValid(username, password){
+for (var i=0; i < database.length; i++) {
+    if (database[i].username && password === database[i].password){
+       return true;
+    }
+    }
+    return false;
+}
 
-function signIn(user, pass){
-    if (user === database[0].username && pass === database[0].password){
+function signIn(username, password){
+     if (isUserValid(username, password)){
         console.log(newsFeed);
     } else{
         alert ("Sorry, wrong username or password!");
     }
 }
+
+let userNamePrompt = prompt("What is your username?");
+let passwordPrompt = prompt("What is your password?");
+
+
+
 
 signIn(userNamePrompt, passwordPrompt);
