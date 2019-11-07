f7a54a0b3afc27bfb0a33ab7de20de4c74d5d2e1
Paige Andrews <pandrews25@yahoo.com>
2019-11-06 19:19:18 -0500

    added favicon

--- game/Quiz2/script.js
+++ game/Quiz2/script.js
@@ -1,4 +1,5 @@
- 
+
+
 function check(){
 
 	let question1 = document.quiz.question1.value;
@@ -41,36 +42,33 @@ function check(){
 		correct++;
 	}
 	
-	let pictures = ["https://pbs.twimg.com/media/C7eHxsZU8AImIxQ.jpg", "https://media1.tenor.com/images/8761b6f3456bf50d2c7d52d08ae86eeb/tenor.gif?itemid=9411057", "https://vignette.wikia.nocookie.net/shingekinokyojin/images/6/67/Miche%27s_death.png/revision/latest?cb=20170917105509"];
-	let messages = ["Great job!", "That's just okay", "You really need to do better"];
+	let pictures = ["https://pbs.twimg.com/media/C7eHxsZU8AImIxQ.jpg", "https://media1.tenor.com/images/8761b6f3456bf50d2c7d52d08ae86eeb/tenor.gif?itemid=9411057", "https://media.giphy.com/media/xUPGcHGUU3d1kYENLq/giphy.gif", "https://media.giphy.com/media/gvRcXbKhO8fBe/giphy.gif"];
+	let messages = ["Great job! You know it all!", "That's just.. okay", "You really need to do better.", "Good Job!! But there's room for improvement."];
 	let score;
 
 	if (correct == 0) {
 		score = 2;
-	}else if (correct > 0 && correct < 8) {
+	}else if (correct > 0 && correct < 5) {
 		score = 1;
 	}else if (correct == 8) {
 		score = 0;
+	}else if (correct == 5 && correct <8){
+		score = 3;
 	}
 
-	//Get the button:
-myButton = document.getElementById("button");
-
-// When the user clicks on the button, scroll to the top of the document
-myButton.onclick = function() {
- topFunction();
-};
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
+	document.getElementById("after_btn").style.visibility = "visible";
 	document.getElementById("after_submit").style.visibility = "visible";
 	document.getElementById("message").innerHTML = messages[score];
 	document.getElementById("number_correct").innerHTML = `You got ${correct} correct.`
 	document.getElementById("picture").src = pictures[score];
-	}
 	
-//    let submitButton = document.getElementById("button");
-//  submitButton.addEventListener("click", check);     
-   
+
+
+}
+
+  function scrollToTop() { 
+	window.location.reload();
+     window.scrollTo(0, 0); 
+        } 	
+  
+  
