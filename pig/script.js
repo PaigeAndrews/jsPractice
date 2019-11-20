5aad011b7266f3d8ac3ea0f11936c65255a3f9b8
Paige Andrews <pandrews25@yahoo.com>
2019-11-19 23:11:53 -0500

    worked on function in pig

--- pig/script.js
+++ pig/script.js
@@ -1,51 +1,55 @@
-player_score = 0
-computer_score = 0
-round_score = 0
-die = 0
-turn = "player"
-roll_again = prompt("Roll the die?")
+player_score = 0;
+computer_score = 0;
+round_score = 0;
+die = null;
+turn = "player";
+roll_again = true;
 
 function game_round(){
 	if (turn == "player"){
-		let roll_again = "yes"
+	     roll_again = "yes"
     }
 		while (roll_again = "yes"){
-			let die = (Math.floor(Math.random() * 6) + 1 )
-	
+			 die = (Math.floor(Math.random() * 6) + 1 )
+            console.log(die + " player die roll");
 			if (die != 1){
 				 round_score += die
             }
 			else{
-				let round_score = 0
+			    round_score = 0
             }
             player_score += round_score
+            console.log(player_score + "player score");
+            console.log(round_score + "round score");
 			roll_again = prompt("Roll again?")
 
             if (roll_again =="no"){
-                let turn = "computer"
+                 turn = "computer"
             }
         }
 	if (turn == "computer"){
-		let roll_again = "yes"
+		 roll_again = "yes"
     }
 		while (roll_again == "yes"){
-			let die = (Math.floor(Math.random() * 6) + 1 )
-        	
+			 die = (Math.floor(Math.random() * 6) + 1 )
+        	console.log(die + " computer die roll")
 
 			if (die != 1){
 				 round_score += die
             }
 			else{
-				let round_score = 0
+				 round_score = 0
             }
 			if (round_score <= 15){
-				let roll_again = "yes"
+				 roll_again = "yes"
             }
 			else{
-				let roll_again = "no"
+				 roll_again = "no"
             }
         }
 		 computer_score += round_score
+          console.log(computer_score + "computer score");
+            console.log(round_score + "round score");
 };
 
 while (computer_score  < 100 && player_score < 100){
