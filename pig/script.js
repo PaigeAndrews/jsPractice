81f08bc7a9ec10c4086a1f6cb374e65eb430177d
Paige Andrews <pandrews25@yahoo.com>
2019-11-20 16:54:51 -0500

    working on functionality

--- pig/script.js
+++ pig/script.js
@@ -3,68 +3,87 @@ computer_score = 0;
 round_score = 0;
 die = null;
 turn = "player";
-roll_again = true;
+roll_again = prompt("Roll?");
 
 function game_round(){
+   
 	if (turn == "player"){
-	     roll_again = "yes"
+	     roll_again
     }
-		while (roll_again = "yes"){
+		while (roll_again == "yes"){
 			 die = (Math.floor(Math.random() * 6) + 1 )
             console.log(die + " player die roll");
+        
 			if (die != 1){
-				 round_score += die
+                 round_score += die
+                 console.log(round_score + "round score");
+                 roll_again = prompt("Roll again?")
             }
-			else{
+			if (die == 1){
 			    round_score = 0
+                turn = "computer"
+                { break; }
             }
-            player_score += round_score
-            console.log(player_score + "player score");
-            console.log(round_score + "round score");
-			roll_again = prompt("Roll again?")
-
             if (roll_again =="no"){
-                 turn = "computer"
+                player_score += round_score
+                console.log(player_score + "player score");
+                console.log(round_score + "round score");
+                round_score = 0
+                turn = "computer"
             }
         }
+            // player_score += round_score
+            // console.log(player_score + "player score");
+            // console.log(round_score + "round score");
+			// roll_again = prompt("Roll again?")
+
+       
+
 	if (turn == "computer"){
+        
 		 roll_again = "yes"
     }
 		while (roll_again == "yes"){
 			 die = (Math.floor(Math.random() * 6) + 1 )
         	console.log(die + " computer die roll")
-
+        
 			if (die != 1){
 				 round_score += die
             }
-			else{
+			if (die == 1){
 				 round_score = 0
+                 turn = "player"
+                 { break; }
             }
-			if (round_score <= 15){
+			if(round_score <= 13){
 				 roll_again = "yes"
             }
-			else{
-				 roll_again = "no"
-            }
+            else{
+                computer_score += round_score
+                console.log(computer_score + "computer score");
+             console.log(round_score + "round score");
+                roll_again = "no"
+                turn = "player"
+           }
         }
-		 computer_score += round_score
-          console.log(computer_score + "computer score");
-            console.log(round_score + "round score");
+           
+           
+             
 };
-
-while (computer_score  < 100 && player_score < 100){
+// game_round()
+while (computer_score  < 100 || player_score < 100){
 	game_round()
 };
 
-while(computer_score == 100 || player_score == 100){
+if (computer_score >= 100 || player_score >= 100){
     end_game()
 };
 
-function endgame(){
-    if (player_score == 100){
+function end_game(){
+    if (player_score >= 100){
         alert("You win!")
     }
-    if (computer_score == 100){
+    if (computer_score >= 100){
         alert("You lose!")
     }
 };
