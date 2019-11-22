3597abd65c08d24ce6e09b270e88e5677a742d1d
Paige Andrews <pandrews25@yahoo.com>
2019-11-22 00:59:14 -0500

    added game ending functionality

--- pig/script.js
+++ pig/script.js
@@ -9,35 +9,32 @@ function player_turn(){
     round_score = 0
     roll_again = "yes"
     
-		while (roll_again == "yes"){
-			die = (Math.floor(Math.random() * 6) + 1 )
-            console.log(`The player rolled a ${die}`)
-        
-			if (die != 1){
-                round_score += die
-                console.log("Player round score: " + round_score);
-                roll_again = prompt("Roll again?")
-            } else {
-                console.log("Player turn over")
-                roll_again = "no"
-		        round_score = 0
-                
-            }
-        }
-        player_score += round_score
-        console.log(player_score + "player score");
-        console.log(round_score + "player round score");
+    while (roll_again == "yes"){
 
-    if(roll_again == "no"){
         if (player_score >= 100){
             alert("You win!")
+        } else if (computer_score >= 100){
+            alert("You lose!")
         }
-        else if (computer_score >= 100){
-        alert("You lose!")
-        } else{
-            computer_turn()
+
+        die = (Math.floor(Math.random() * 6) + 1 )
+        console.log(`The player rolled a ${die}`)
+    
+        if (die != 1){
+            round_score += die
+            console.log("Player round score: " + round_score);
+            roll_again = prompt("Roll again?")
+        } else {
+            console.log("Player turn over")
+            roll_again = "no"
+            round_score = 0
         }
     }
+    player_score += round_score
+    console.log(player_score + "player score");
+    console.log(round_score + "player round score");
+    turn = "computer"
+    //computer_turn()
     
         
 };
@@ -48,37 +45,37 @@ function computer_turn(){
     turn = "computer"
     roll_again = "yes"
 
-		while (roll_again == "yes"){
-			die = (Math.floor(Math.random() * 6) + 1 )
-        	console.log(die + " computer die roll")
-        
-			if (die != 1){
-				round_score += die
-            } else {
-				round_score = 0
-                roll_again = "no"
-            }
+    while (roll_again == "yes"){
 
-            if (round_score > 14){
-                computer_score += round_score
-                console.log(computer_score + "computer score");
-                console.log(round_score + "computer round score");
-                roll_again = "no"
-            } 
-        }
-        computer_score += round_score
-        console.log(computer_score + "computer score");
-        console.log(round_score + "computer round score"); 
-
-    if(roll_again == "no"){
         if (player_score >= 100){
             alert("You win!")
+            break
+        } else if (computer_score >= 100){
+            alert("You lose!")
+            break
+        } 
+
+        die = (Math.floor(Math.random() * 6) + 1 )
+        console.log(die + " computer die roll")
+    
+        if (die != 1){
+            round_score += die
+        } else {
+            round_score = 0
+            roll_again = "no"
         }
-        else if (computer_score >= 100){
-        alert("You lose!")
+
+        if (round_score > 14){
+            computer_score += round_score
+            console.log(computer_score + "computer score");
+            console.log(round_score + "computer round score");
+            roll_again = "no"
         } 
     }
-        
+    computer_score += round_score
+    console.log(computer_score + "computer score");
+    console.log(round_score + "computer round score"); 
+    turn = "player"
 };
 
 // function end_game(){
@@ -91,12 +88,21 @@ function computer_turn(){
 // };
 
 
-while (computer_score  < 100 || player_score < 100){
-	player_turn()
+// while (computer_score  < 100 || player_score < 100){
+// 	player_turn()
+// };
+
+
+while (computer_score  < 100 && player_score < 100){
+    if (turn == "player"){
+        player_turn()
+    } else {
+        computer_turn()
+    }
 };
-
-// end_game()
-
+console.log("The end.")
 
 
-
+// while(false && true){
+//     //run code
+// }
