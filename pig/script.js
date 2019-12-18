6b4a360e69d851d5791cc416541a0b73f389cfb1
Paige Andrews <pandrews25@yahoo.com>
2019-12-17 22:59:07 -0500

    changed variable names throughout code and and started queryselectors to hook up javascript to pig game

--- pig/script.js
+++ pig/script.js
@@ -1,91 +1,109 @@
-// player_score = 0;
-// computer_score = 0;
-// round_score = 0;
+let playerTotalScoreDisplay = document.querySelector("#playerTotalScore")
+let computerTotalScoreDisplay = document.querySelector("#computerTotalScore")
+let playerRoundScoreDisplay = document.querySelector("#playerRoundScoreDisplay")
+let computerRoundScoreDisplay = document.querySelector("#computerRoundScoreDisplay")
+let rollButton = document.querySelector("#rollDie")
+let holdButton = document.querySelector("#hold")
+
+
+let mainPictures = ["D:\Pictures\1-\human.png", "D:\Pictures\1-\humanTWO.png", "D:\Pictures\1-\robot-face-6-1074719.png",
+"D:\Pictures\1-\robot-face-6-1074719TWO.png"];
+
+let dicePictures =["D:\Pictures\1-\die1.png", "D:\Pictures\1-\2.png", "D:\Pictures\1-\3.png",
+"D:\Pictures\1-\4.png", "D:\Pictures\1-\5.png", "D:\Pictures\1-\6.png"
+];
+
+roll_btn.addEventListener("click", playerTurn);
+
+
+// playerScore = 0;
+// computerScore = 0;
+// roundScore = 0;
 // die = null;
 // turn = "player";
-// roll_again = null;
+// rollAgain = null;
 
-// function player_turn(){
-//     round_score = 0
-//     roll_again = prompt("Roll?")
+// function playerTurn(){
+//     roundScore = 0
+//     rollAgain = prompt("Roll?")
     
-//     while (roll_again == "yes"){
+//     while (rollAgain == "yes"){
 
      
 //         die = (Math.floor(Math.random() * 6) + 1 )
 //         console.log(`The player rolled a ${die}`)
-    
-//         if (die != 1){
-//             round_score += die
 
-//             if (player_score + round_score >= 100){
+//         if (die != 1){
+//             roundScore += die
+
+//             if (playerScore + roundScore >= 100){
 //             alert("You win!")
 //             break
 //            }
-
-//             console.log("Player round score: " + round_score);
-//             roll_again = prompt("Roll again?")
+//            playerScore.innerHTML = playerScore    
+//             console.log("Player round score: " + roundScore);
+//             rollAgain = prompt("Roll again?")
 //         } else {
 //             console.log("Player turn over")
-//             roll_again = "no"
-//             round_score = 0
+//             rollAgain = "no"
+//             roundScore = 0
 //         }
 
         
 //     }
      
-//     player_score += round_score
-//     console.log(player_score + "player score");
-//     console.log(round_score + "player round score");
+//     playerScore += roundScore
+//     console.log(playerScore + "player score");
+//     console.log(roundScore + "player round score");
 //     turn = "computer"
         
 // };
 
 
-// function computer_turn(){
-//     round_score = 0
+// function computerTurn(){
+//     roundScore = 0
 //     turn = "computer"
-//     roll_again = "yes"
+//     rollAgain = "yes"
 
-//     while (roll_again == "yes"){
+//     while (rollAgain == "yes"){
 
        
 //         die = (Math.floor(Math.random() * 6) + 1 )
 //         console.log(die + " computer die roll")
     
 //         if (die != 1){
-//             round_score += die
-//             if (computer_score + round_score >= 100){
+//             roundScore += die
+//             if (computerScore + roundScore >= 100){
 //             alert("You lose!")
 //             break
 //             }
 //         } else {
-//             round_score = 0
-//             roll_again = "no"
+//             roundScore = 0
+//             rollAgain = "no"
 //         }
 
-//         if (round_score > 14){
-//             if (computer_score + round_score >= 100){
+//         if (roundScore > 14){
+//             if (computerScore + roundScore >= 100){
 //             alert("You lose!")
 //             }
-//             console.log(computer_score + "computer score");
-//             console.log(round_score + "computer round score");
-//             roll_again = "no"
+//             console.log(computerScore + "computer score");
+//             console.log(roundScore + "computer round score");
+//             rollAgain = "no"
 //         } 
 
 //     }
-//     computer_score += round_score
-//     console.log(computer_score + "computer score");
-//     console.log(round_score + "computer round score"); 
+//     computerScore += roundScore
+//     console.log(computerScore + "computer score");
+//     console.log(roundScore + "computer round score"); 
 //     turn = "player"
 
 // };
 
-// while (computer_score  < 100 && player_score < 100){
+// while (computerScore  < 100 && playerScore < 100){
 //     if (turn == "player"){
-//         player_turn()
+//         playerTurn()
 //     } else {
-//         computer_turn()
+//         computerTurn()
 //     }
 // };
 
