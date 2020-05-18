fdc38fb68d762c0374f3cb8eb27461692bc6e984
Paige Andrews <pandrews25@yahoo.com>
2020-05-18 19:08:57 -0400

    added modal and changed some js logic

--- hangman/script.js
+++ hangman/script.js
@@ -99,6 +99,11 @@ let inputField = document.querySelector(".inputField")
 let instructions = document.querySelector(".instructions")
 let underscores = document.querySelector(".underscores")
 let hangman = document.querySelector(".hangman")
+let playAgain = document.getElementById("playAgain")
+let modal = document.getElementById("modal")
+let easyGame = document.getElementById("easy")
+let mediumGame = document.getElementById("medium")
+let hardGame = document.getElementById("hard")
 let lives = 6
 
 
@@ -114,14 +119,36 @@ for (word of wordList){
   }
 }
 
+easyGame.addEventListener("click",function(){
+  difficulty = "easy"
+  modal.style.display = "none"
+  wordChooser(easyList, mediumList, hardList)
+  // console.log("easy")
+})
+
+mediumGame.addEventListener("click",function(){
+  difficulty = "medium"
+  modal.style.display = "none"
+  wordChooser(easyList, mediumList, hardList)
+  // console.log("medium")
+})
+
+hardGame.addEventListener("click",function(){
+  difficulty = "hard"
+  modal.style.display = "none"
+  wordChooser(easyList, mediumList, hardList)
+  // console.log("hard")
+})
 
 function wordChooser(easyList, mediumList, hardList){
-  difficulty = prompt("Easy, Medium, or Hard").toLowerCase()
-  
-  while(difficulty != "easy" && difficulty != "medium" && difficulty != "hard"){
-    difficulty = prompt("Your choice must be Easy, Medium, or Hard").toLowerCase()
-  }
+  // modal.style.display = "block"
 
+  // difficulty = prompt("Easy, Medium, or Hard").toLowerCase()
+  
+  // while(difficulty != "easy" && difficulty != "medium" && difficulty != "hard"){
+  //   difficulty = prompt("Your choice must be Easy, Medium, or Hard").toLowerCase()
+  // }
+  console.log(difficulty)
   if(difficulty == "easy"){
     randomWord = easyList[Math.floor(Math.random() * easyList.length)]
   } else if (difficulty == "medium") {
@@ -129,34 +156,30 @@ function wordChooser(easyList, mediumList, hardList){
   } else {
     randomWord = hardList[Math.floor(Math.random() * hardList.length)]
   }
-  
   randomWord = randomWord.split("")
   activeWordFunction()
 }
 
 
 function activeWordFunction(){
+  
   for(n=0; n < randomWord.length; n++) {
     indexesOfMyWord.push("_")
   } 
-  
   underscores.innerHTML = indexesOfMyWord;
 }
 
 function replacingLetter(){
   for(i=0; i <= randomWord.length - 1; i++){
-    
     if(randomWord[i] == inputField.value.toLowerCase()){
       indexesOfMyWord[i] = inputField.value
     }
   }
-
   underscores.innerHTML = indexesOfMyWord
   instructionsDisplay()
 }
 
 function instructionsDisplay(){
-
   if (usedLetters.includes(inputField.value)){
     instructions.innerHTML ="You already guessed \"" + inputField.value + "\"!";
   }else if (inputField.value.length >=2){
@@ -169,39 +192,40 @@ function instructionsDisplay(){
     wrongLetters.push(inputField.value)
     instructions.innerHTML = `Oops! This letter is not in the word <br> Guess another letter! <br> Youve used these letters: ${usedLetters} ${wrongLetters}`
     lives--
-    
   }
   winLose()
 }
    
 function winLose(){
-  
     if(indexesOfMyWord.includes("_") != true){
        inputField.style.display = "none"
        instructions.innerHTML = "You win! Congratulations!!"
        underscores.innerHTML = `${randomWord.join('')}`
+       playAgain.style.display = "inline-block"
     }else if(lives <= 0){
       instructions.innerHTML = `Sorry, you lose! The word was ${randomWord.join('')}`
-
+      playAgain.style.display = "inline-block"
       inputField.style.display = "none"
-    }  
-    
+    } 
     hangman.innerHTML = `Lives: ${lives}`
 }
 
 
-wordChooser(easyList, mediumList, hardList)
-
 
 inputField.addEventListener("keydown",function(event){
   if(event.which == "13"){
-  
     replacingLetter()
-
     inputField.value = ""
   }
-  
 })
+
+playAgain.addEventListener("click",function(){
+  location.reload();
+    return false;
+})
+
+
+
 
 console.log("underscores", indexesOfMyWord, "random word", randomWord)
 
