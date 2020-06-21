23c06f6626d3d5a1c046b44539b9ca1e8594cc09
Paige Andrews <pandrews25@yahoo.com>
2020-06-20 23:52:58 -0400

    added js challenge

--- jsChallenges/1/script.js
+++ jsChallenges/1/script.js
@@ -589,3 +589,8 @@ function countTrue(arr) {
 	}
 	return count
 }
+
+// Write a function that converts an object into an array, where each element represents a key-value pair.
+function toArray(obj) {
+	return Object.entries(obj);
+}
