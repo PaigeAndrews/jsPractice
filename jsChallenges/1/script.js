161c88e3996833e8bd65bad7d91d8563651d056b
Paige Andrews <pandrews25@yahoo.com>
2020-06-28 23:30:07 -0400

    daily js challenge

--- jsChallenges/1/script.js
+++ jsChallenges/1/script.js
@@ -615,3 +615,9 @@ function getBudgets(arr) {
 	}
 	return result
 }
+
+// Write a function that converts an object into an array of keys and values.
+function objectToArray(obj) {
+	const myArray = Object.entries(obj);
+	return myArray;
+}
