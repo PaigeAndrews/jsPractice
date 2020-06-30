f9ad29eed1b7c9381016402adbe888155f812887
Paige Andrews <pandrews25@yahoo.com>
2020-06-29 22:09:12 -0400

    daily js challenge

--- jsChallenges/1/script.js
+++ jsChallenges/1/script.js
@@ -621,3 +621,15 @@ function objectToArray(obj) {
 	const myArray = Object.entries(obj);
 	return myArray;
 }
+
+// The Code tab has a code which attempts to add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code?
+
+// Examples
+// clone([1, 2, 3]) ➞ [1, 2, 3, [1, 2, 3]]
+
+// clone(["x", "y"]) ➞ ["x", "y", ["x", "y"]]
+function clone(arr) {
+	let arr2 = arr.map(x => x)
+	arr.push(arr2)
+	return arr
+}
